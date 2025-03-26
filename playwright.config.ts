ca1c873cdcd7229ff9220779f2a6c95a76b7f0a6
Danilo da Rosa Oliveira <danilodarosaoliveira@gmail.com>
2025-03-26 16:27:23 -0300

    Headless mode true

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests', // Diretório onde os testes serão encontrados
   use: {
-    headless: false, // Altere para true se quiser rodar em segundo plano (sem abrir o navegador)
+    headless: true, // Altere para true se quiser rodar em segundo plano (sem abrir o navegador)
     viewport: { width: 1280, height: 720 }, // Resolução do navegador
     screenshot: 'only-on-failure', // Captura de tela em caso de falha
     video: 'retain-on-failure', // Gravação de vídeo apenas em falhas
