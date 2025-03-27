62070bb3147b7443193cdb393093870340dd64d3
Danilo da Rosa Oliveira <danilodarosaoliveira@gmail.com>
2025-03-27 11:52:44 -0300

    Adding more tests and testing owners

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 
 const API_URL = 'https://reqres.in/api';
 
@@ -38,4 +38,17 @@ test('DELETE - remove a user', async ({ request }) => {
     const response = await request.delete(`${API_URL}/users/2`);
 
     expect(response.status()).toBe(204); 
+});
+
+test('alpha test', async ({ request }) => {
+    const city = 'London';
+    const id = '7301e3e91cb259dad0c62158ecd2ac34';
+    const response = await request.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}`);
+
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    console.log(responseBody);
+
+    expect(responseBody.name).toBe(city);
+    expect(responseBody.main.temp).toBeGreaterThanOrEqual(0);
 });
